b8e421bc15d20afcea4e9a52ec098cf8df4a9da0
Olena Daly <olena.daly@gmail.com>
2025-07-06 10:52:18 +1000

    removed extra code challenges

--- robot_grid/robot.test.ts
+++
@@ -1,7 +0,0 @@
-import { Robot } from "./robot";
-const robot = new Robot(6);
-
-test('robot move UUDDLR', () => {
-        robot.move('UUDDLR');
-        expect(robot.getPosition()).toEqual([1,0]);
-    });
